5ff988dd1965dc4776a56b283268bf16c3c0df6e
Aasif Saneeth <saneeth@titumlk.onmicrosoft.com>
2025-03-21 20:26:50 +0530

    select and select all query

--- libs/App.php
+++ libs/App.php
@@ -25,6 +25,35 @@ class App {
         echo "db connectiion is working";
     }
    }
+
+   //select all 
+
+   public function selectAll($query){
+        $rows = $this->link->query($query);
+        $rows->execute();
+
+        $allRows = $rows->fetchall(PDO::FETCH_OBJ);
+
+        if($allRows){
+            return $allRows;
+        }else {
+            return false;
+        }
+   }
+
+//select one row
+   public function selectOne($query){
+    $row = $this->link->query($query);
+    $row->execute();
+
+    $singleRow = $row->fetch(PDO::FETCH_OBJ);
+
+    if($singleRow){
+        return $singleRow;
+    }else {
+        return false;
+    }
+}
 }
 
 $obj = new App;
